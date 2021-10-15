0776e60068a3a1a51088189f7aff08b7bdab2603
ALINE <hoshinoaline@yahoo.com.br>
2021-10-15 13:53:07 -0300

    Updates

---
+++ precache-manifest.d217be2f45747ab7e3a51d378e938708.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f97c97ce4094f8d9c954809796513ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ae7be954f89deed278",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "d1b31a0886906acd1b43",
+    "url": "/static/css/main.afe2c011.chunk.css"
+  },
+  {
+    "revision": "95ae7be954f89deed278",
+    "url": "/static/js/2.c7267d6d.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/static/js/2.c7267d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b31a0886906acd1b43",
+    "url": "/static/js/main.6866a321.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "cca26527a87fb615a13dfe6d93da5f52",
+    "url": "/static/media/LogoLanchinho-logo.cca26527.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "2c1eaa2748ec9c801cdced5d016572be",
+    "url": "/static/media/chapeuchef543.2c1eaa27.png"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
